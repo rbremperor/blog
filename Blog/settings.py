c93fe43be16f0448597f7c22e07d8acf8857ac05
Raxmatillo Burxonov <150665635+rbremperor@users.noreply.github.com>
2025-04-11 10:03:49 +0500

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -122,9 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"  # URL to access static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # Additional static file locations
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -133,3 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://auto-bidding-production.up.railway.app",
+]
